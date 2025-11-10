8c4a3901091717efdda2170f9cbf78989dd1b052
mulirous <muriloalimacosta@hotmail.com>
2025-11-10 03:18:59 -0300

    feat: implementa lógica de negócio para ServiceMedia

--- src/main/java/com/pertindetu/dev/repositories/ServiceMediaRepository.java
+++ src/main/java/com/pertindetu/dev/repositories/ServiceMediaRepository.java
@@ -1,9 +1,15 @@
 package com.pertindetu.dev.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pertindetu.dev.models.ServiceMedia;
 
 public interface ServiceMediaRepository extends JpaRepository<ServiceMedia, Long> {
-
+  
+  List<ServiceMedia> findByServiceIdOrderByOrderAsc(Long serviceId);
+  
+  void deleteByServiceId(Long serviceId);
+  
 }
